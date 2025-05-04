cf733107108679d522814161a42ffbd7a6158541
Feddy's Thinkpad <cavaliere12.web@gmail.com>
2025-05-04 15:39:36 +0200

    mock data

--- src/api.ts
+++ src/api.ts
@@ -1,42 +1,53 @@
 import { Record } from "./idb"
-interface APIRecord {
-    author: string
-    category: string
-    classgroup: number //sequence number of group
-    creation_date: Date
-    domain: string //domain or website
-    err_message: null
-    expiry_date: Date //put by the CMS
-    heading: string
-    id: number
-    image: string
-    override: string //override title
-    slug: string //alternative ID
-    status: "fetched" //must be
-    subtitle: string
-    title: string
-    update_date: Date //of the CMS
-    vimeo: {
-        static_url: string
-        duration: number
-        expires: Date
-    }
-}
+import * as constants from "./constants"
 
+/**
+ * Records are descriptive of the resource
+ * @returns 
+ */
 
-export const totalFetch = async (): Promise<Record[]> => fetch(
-        'http://192.168.0.238:8989/api/cache/onair?type=video&vimeo=true',
-        { credentials: 'include' })
-    .then( res => res.status==200 ? res.json() : Promise.reject(res.json()) )
-    .then( debug => {
-        console.log(debug)
-        return debug
-    })
-    .then( (data: APIRecord[]) => data.map( (v, index) => ({
-        id: v.id.toString(),
-        url: v.vimeo.static_url,
-        title: v.title,
-        subtitle: v.subtitle,
-        status: true,
-        sequence: index
-    })))
+export const totalFetch = async (type: "video"|"image"): Promise<Record[]> => [{
+    id: "011",
+    sequence: 1,
+    title: "video11",
+    subtitle: "This is video 1",
+    status: true,
+    contentType: type,
+    url: "samples/video11.mp4",
+    date: new Date(),
+    updated: new Date(),
+    expiry: new Date((new Date()).getTime() + 3 * 60 * 60 * 1000)
+} as Record, {
+    id: "012",
+    sequence: 2,
+    title: "video12",
+    subtitle: "This is video 2",
+    status: true,
+    contentType: type,
+    url: "samples/video12.mp4",
+    date: new Date(),
+    updated: new Date(),
+    expiry: new Date((new Date()).getTime() + 3 * 60 * 60 * 1000)
+} as Record, {
+    id: "013",
+    sequence: 3,
+    title: "video13",
+    subtitle: "This is video 3",
+    status: true,
+    contentType: type,
+    url: "samples/video13.mp4",
+    date: new Date(),
+    updated: new Date(),
+    expiry: new Date((new Date()).getTime() + 3 * 60 * 60 * 1000)
+} as Record, {
+    id: "014",
+    sequence: 4,
+    title: "video14",
+    subtitle: "This is video 4",
+    status: true,
+    contentType: type,
+    url: "samples/video14.mp4",
+    date: new Date(),
+    updated: new Date(),
+    expiry: new Date((new Date()).getTime() + 3 * 60 * 60 * 1000)
+} as Record]
--- src/event.ts
+++ src/event.ts
@@ -1,35 +1,21 @@
-import { APIRecord, dataSynthesis, singleFetch, sortedList } from './api.js';
-import { Playlist } from './idb.js';
+/**
+ * any event triggers a call to the playlist element
+ * or eventually can request a double check to the API
+ */
+
+// import { APIRecord, sortedList } from './api';
+import { Playlist } from './idb';
 
 export interface UpdateEvent {
-    domain: string
-    id: number
+    uuid: string
+    domain?: string
+    id?: number
+    update_date: Date
+    classgroup?: number
     status?: boolean
     title?: string
-}
-
-export const update = (p: Playlist, ev: UpdateEvent)=>{
-    return p.get(`${ev.domain}@${ev.id}`)
-    .then( async record => {
-        if (record !== null)
-            return p.update({
-                ...record,
-                status: ev.status!=undefined ? ev.status : record.status,
-                title: ev.title!=undefined ? ev.title : record.title  // per esempio
-            })
-        else 
-            return singleFetch(ev.domain, ev.id)
-            .then(function(toCache){p.cache(toCache)})  // will be cached as disabled, but after a sort will be done
-    })
-    .then( sortedList )  // from cloud api
-    .then( playlist => Promise.all(playlist.map( (v,i) => p.updateSequence(v.id, i+1) )) )  //seq minimo =1
-}
-
-export const insert = (p: Playlist, ev: APIRecord)=>{
-    return p.cache({
-        ...dataSynthesis(ev),
-        status: false
-    })
-    .then( sortedList )  // from cloud api
-    .then( playlist => Promise.all(playlist.map( (v,i) => p.updateSequence(v.id, i+1) )) )  //seq minimo =1
+    override?: string
+    subtitle?: string
+    heading?: string
+    expiry_date?: string
 }
